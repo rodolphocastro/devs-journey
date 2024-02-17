b89c17fd8fbb415a3cfdc2f5759a02f8e9b61496
Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>
2024-02-16 22:00:47 -0300

    feat: init core service

---
+++ dev-journey-core/src/main/kotlin/br/dev/ardc/devjourneycore/DevJourneyCoreApplication.kt
@@ -0,0 +1,11 @@
+package br.dev.ardc.devjourneycore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevJourneyCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<DevJourneyCoreApplication>(*args)
+}
---
+++ dev-journey-core/src/test/kotlin/br/dev/ardc/devjourneycore/DevJourneyCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.dev.ardc.devjourneycore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevJourneyCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
