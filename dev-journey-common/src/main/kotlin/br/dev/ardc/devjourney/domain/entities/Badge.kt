2af139cf9bb66d6f61527ddf320913d2ae96a4f2
Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>
2024-02-18 21:25:49 -0300

    refactor: move EarnedBadge to its own file

--- dev-journey-common/src/main/kotlin/br/dev/ardc/devjourney/domain/entities/Badge.kt
+++ dev-journey-common/src/main/kotlin/br/dev/ardc/devjourney/domain/entities/Badge.kt
@@ -1,8 +1,5 @@
 package br.dev.ardc.devjourney.domain.entities
 
-import java.time.LocalDate
-import java.util.UUID
-
 /**
  * Contract that a badge must implement.
  */
@@ -17,41 +14,3 @@ interface IBadge {
  * @param description the description of the badge, may be blank
  */
 data class Badge(override val title: String, override val description: String) : IBadge
-
-/**
- * A badge represents an achievement or a milestone that a dev has earned on its Journey.
- * @param id the unique identifier of the badge
- * @param title the title of the badge
- * @param description the description of the badge, may be blank
- * @param earnedAt the date when the badge was earned, cannot be in the future
- */
-class EarnedBadge(
-    val id: UUID,
-    val assignedToUser: UUID,
-    override val title: String,
-    override val description: String,
-    val earnedAt: LocalDate
-) : IBadge {
-    init {
-        require(title.isNotBlank()) { "Title cannot be blank" }
-        require(earnedAt.isBefore(LocalDate.now().plusDays(1))) { "Date cannot be in the future" }
-    }
-
-    companion object {
-        /**
-         * Creates a new badge with a random UUID
-         * @param title the title of the badge
-         * @param description the description of the badge
-         * @param earnedAt the date when the badge was earned
-         */
-        fun create(title: String, assignedToUser: UUID, description: String, earnedAt: LocalDate): EarnedBadge {
-            return EarnedBadge(
-                UUID.randomUUID(),
-                assignedToUser,
-                title,
-                description,
-                earnedAt
-            )
-        }
-    }
-}
